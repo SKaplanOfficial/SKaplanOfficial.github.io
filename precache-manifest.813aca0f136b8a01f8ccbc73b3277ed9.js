ed5e1f488d9fb4ea64e550f0eb211941af6ff33a
Stephen Kaplan <stephen.kaplan@maine.edu>
2020-01-01 03:15:10 -0500

    Updates

---
+++ precache-manifest.813aca0f136b8a01f8ccbc73b3277ed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5c7a7d4897581ffa9b77184f05649d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7254e274a7911f256991",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c03c582b2716db8b8354",
+    "url": "/static/js/2.a2203b5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a2203b5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7254e274a7911f256991",
+    "url": "/static/js/main.30c2fe1e.chunk.js"
+  },
+  {
+    "revision": "766d00e5e0ae07afecf1",
+    "url": "/static/js/runtime-main.a1554be4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
